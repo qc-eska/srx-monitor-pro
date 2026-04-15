68d9f152d4072e09820f5b61889d640416743eb2
qc-eska <qc@telcon.pl>
2026-04-15 19:18:14 +0200

    Update olx.py

--- scrapers/olx.py
+++ scrapers/olx.py
@@ -6,26 +6,28 @@ urls = [
     "https://www.olx.pl/motoryzacja/samochody/q-honda-element/"
 ]
 
+
 def fetch_olx():
     headers = {
         "User-Agent": "Mozilla/5.0"
     }
 
-    r = requests.get(URL, headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
-
     listings = []
 
-    for item in soup.select("div[data-cy='l-card']"):
-        title = item.select_one("h6")
-        price = item.select_one("p[data-testid='ad-price']")
-        link = item.select_one("a")
+    for URL in urls:
+        r = requests.get(URL, headers=headers)
+        soup = BeautifulSoup(r.text, "lxml")
 
-        if title and link:
-            listings.append({
-                "title": title.text.strip(),
-                "price": price.text.strip() if price else "brak",
-                "url": "https://www.olx.pl" + link["href"],
-            })
+        for item in soup.select("div[data-cy='l-card']"):
+            title = item.select_one("h6")
+            price = item.select_one("p[data-testid='ad-price']")
+            link = item.select_one("a")
+
+            if title and link:
+                listings.append({
+                    "title": title.text.strip(),
+                    "price": price.text.strip() if price else "brak",
+                    "url": "https://www.olx.pl" + link["href"],
+                })
 
     return listings
