f8e8fb14d7a582b620d2f47a414e0ff0d13a2680
qc-eska <qc@telcon.pl>
2026-04-15 19:19:05 +0200

    Update otomoto.py

--- scrapers/otomoto.py
+++ scrapers/otomoto.py
@@ -1,28 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.otomoto.pl/osobowe/cadillac/srx/"
+urls = [
+    "https://www.otomoto.pl/osobowe/cadillac/srx/",
+    "https://www.otomoto.pl/osobowe/honda/element/"
+]
+
 
 def fetch_otomoto():
     headers = {
         "User-Agent": "Mozilla/5.0"
     }
 
-    r = requests.get(URL, headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
-
     listings = []
 
-    for item in soup.select("article"):
-        title = item.select_one("h2")
-        price = item.select_one("span")
-        link = item.select_one("a")
+    for URL in urls:
+        r = requests.get(URL, headers=headers)
+        soup = BeautifulSoup(r.text, "lxml")
 
-        if title and link:
-            listings.append({
-                "title": title.text.strip(),
-                "price": price.text.strip() if price else "brak",
-                "url": link["href"],
-            })
+        for item in soup.select("article"):
+            title = item.select_one("h2")
+            price = item.select_one("span")
+            link = item.select_one("a")
+
+            if title and link:
+                listings.append({
+                    "title": title.text.strip(),
+                    "price": price.text.strip() if price else "brak",
+                    "url": link["href"],
+                })
 
     return listings
