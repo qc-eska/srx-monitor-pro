6481aa31e17e6dda08313c23c44dfdcf0f61b398
qc-eska <qc@telcon.pl>
2026-04-15 19:19:59 +0200

    Update autoplac.py

--- scrapers/autoplac.py
+++ scrapers/autoplac.py
@@ -1,24 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://autoplac.pl/szukaj/cadillac/srx"
+urls = [
+    "https://autoplac.pl/szukaj/cadillac/srx",
+    "https://autoplac.pl/szukaj/honda/element"
+]
+
 
 def fetch_autoplac():
     headers = {"User-Agent": "Mozilla/5.0"}
-    r = requests.get(URL, headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
 
     listings = []
 
-    for item in soup.select("a"):
-        href = item.get("href", "")
-        text = item.get_text(" ", strip=True)
+    for URL in urls:
+        r = requests.get(URL, headers=headers)
+        soup = BeautifulSoup(r.text, "lxml")
 
-        if "srx" in text.lower() and href:
-            listings.append({
-                "title": text,
-                "price": "brak",
-                "url": href,
-            })
+        for item in soup.select("a"):
+            href = item.get("href", "")
+            text = item.get_text(" ", strip=True)
+
+            if href and text:
+                listings.append({
+                    "title": text,
+                    "price": "brak",
+                    "url": href,
+                })
 
     return listings
